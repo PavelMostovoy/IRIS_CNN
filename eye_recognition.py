3335748f5bddd9887ada1bcb45f61e53b6d00d85
Pavel Mostovoy <pavel.mostovoy@msn.com>
2017-11-27 13:39:14 +0200

    next try to find iris using openCV with circle recognition

--- eye_recognition.py
+++ eye_recognition.py
@@ -22,8 +22,11 @@ image = cv2.imread("picture.png")
 output = image.copy()
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+#cv2.imshow("gray", gray)
+
+
 # detect circles in the image
-circles = cv2.HoughCircles(gray , cv2.HOUGH_GRADIENT , 1.8 , 2)
+circles = cv2.HoughCircles(gray , cv2.HOUGH_GRADIENT, 2.1, 200,param1=128,  param2=64,minRadius=20, maxRadius=100) #  param1=128,param2=64, minRadius=1, maxRadius=30
 #circles = cv2.HoughCircles(gray, cv2.HoughCircles, 1.2, 100)
 # ensure at least some circles were found
 if circles is not None:
@@ -35,10 +38,13 @@ if circles is not None:
         # draw the circle in the output image, then draw a rectangle
         # corresponding to the center of the circle
         cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-        cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+        #cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
 
     # show the output image
     cv2.imshow("output", np.hstack([image, output]))
+    cv2.waitKey(0)
+    
+else:
     cv2.waitKey(0)
 
 
