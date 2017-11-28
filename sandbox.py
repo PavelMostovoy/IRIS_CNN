6b46a1a3f75264de74d7c029708ca5909d50918d
Pavel Mostovoy <pavel.mostovoy@msn.com>
2017-11-28 12:02:30 +0200

    next try to find iris using openCV with circle recognition

--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 
 # 187  84  64  64 || 76  82  63  63
 
@@ -7,14 +6,9 @@ x, y, w, h = 187,  84,  64,  64,
 #x,y,w,h = 76,  82,  63,  63
 
 img = cv2.imread('picture3.png')
-
 img2= img.copy()
-
 img = img[y:y+h,x:x+w] # NOTE: its img[y: y + h, x: x + w]
-
 cv2.imwrite("eye.png",img)
-
 cv2.imshow('img', img)
-
 cv2.waitKey(0)
 cv2.destroyAllWindows()
