77b3ea50ee85e58530ded532fd540be62bfa9f80
Pavel Mostovoy <pavel.mostovoy@msn.com>
2017-11-28 11:38:07 +0200

    next try to find iris using openCV with circle recognition

--- eye_recognition.py
+++ eye_recognition.py
@@ -6,11 +6,14 @@ eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 img = cv2.imread('picture3.png')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-eye_1 = eye_cascade.detectMultiScale(gray,1.3,5)
-print(eye_1)
-for (ex, ey, ew, eh) in eye_1:
-
-    cv2.rectangle(img, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+eye = eye_cascade.detectMultiScale(gray, 1.3, 5)
+print(eye)
+for (x, y, w, h) in eye:
+    name = str(x+y)
+    t_img=img.copy()
+    t_img = t_img[y:y + h, x:x + w]
+    cv2.imwrite("{file_mane}.png".format(file_mane=name ), t_img)
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 cv2.imshow('img', img)
 cv2.waitKey(0)
--- sandbox.py
+++ sandbox.py
@@ -1,45 +1,20 @@
 import cv2
 import numpy as np
 
-def i_convertor(image):
-    image = cv2.imread(image)
-    image = cv2.medianBlur(image, 5)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image_test = cv2.Canny(image,80,50,apertureSize=3)
-    image_test = cv2.GaussianBlur(image_test, (5,5), 1)
-    return image_test
+# 187  84  64  64 || 76  82  63  63
 
+x, y, w, h = 187,  84,  64,  64,
+#x,y,w,h = 76,  82,  63,  63
 
+img = cv2.imread('picture3.png')
 
-cv2.imshow("gray", i_convertor("picture.png"))
+img2= img.copy()
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+img = img[y:y+h,x:x+w] # NOTE: its img[y: y + h, x: x + w]
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
-img = cv2.imread('picture.png')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-eye_1 = eye_cascade.detectMultiScale(gray,1.3,5)
-print(eye_1)
-for (ex, ey, ew, eh) in eye_1:
-
-    cv2.rectangle(img, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+cv2.imwrite("eye.png",img)
 
 cv2.imshow('img', img)
-cv2.waitKey(0)
 
-
-'''faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-cv2.imshow('img',img)
 cv2.waitKey(0)
-'''
 cv2.destroyAllWindows()
