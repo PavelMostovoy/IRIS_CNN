db58db431aea1f081822cf7fbc9078a9a27303a7
Pavel Mostovoy <pavel.mostovoy@msn.com>
2017-12-11 17:40:06 +0200

    P.Mostovoy. rework for 2 clases

--- cnn.py
+++ cnn.py
@@ -41,11 +41,11 @@ classifier.add(Flatten())
 
 # Step 4 - Full connection
 classifier.add(Dense(units = 128, activation = 'relu'))
-classifier.add(Dense(units = 1, activation = 'sigmoid')) # sigmoid for binary outcome softmax fpr more than 2 units
+classifier.add(Dense(units = 2, activation = 'softmax')) # sigmoid for binary outcome softmax fpr more than 2 units
 
 
 # Compiling the CNN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])#cathegorica_crosentropy
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])#categorical_crosentropy
 
 
 # Part 2 - Fitting the CNN to the images
@@ -62,12 +62,12 @@ test_datagen = ImageDataGenerator(rescale = 1./255)
 training_set = train_datagen.flow_from_directory('dataset/training_face',
                                                  target_size = (img_dim_x, img_dim_y),
                                                  batch_size = 4,
-                                                 class_mode = 'binary') # save_to_dir="dataset/augemented" - this could show generated files
+                                                 class_mode = 'categorical') # save_to_dir="dataset/augemented" - this could show generated files
 
 test_set = test_datagen.flow_from_directory('dataset/test_face',
                                             target_size = (img_dim_x, img_dim_y),
                                             batch_size =4,
-                                            class_mode = 'binary')# was binary can be sparse
+                                            class_mode = 'categorical')# was binary can be sparse
 
 classifier.fit_generator(training_set,
                          steps_per_epoch = 4000,
@@ -75,7 +75,7 @@ classifier.fit_generator(training_set,
                          validation_data = test_set,
                          validation_steps = 400) # steps per epoch -  Total number of steps (batches of samples)
 
-classifier.save("model_face_2.h5")   # save model for future purposes
+classifier.save("model_face_cat.h5")   # save model for future purposes
 
 import numpy as np
 from keras.preprocessing import image
--- cnn3_solution.py
+++ cnn3_solution.py
@@ -2,13 +2,13 @@
 import image from directory
 and use previously generated model for clarification
 """
-
+import cv2
 import keras
 import numpy as np
 from keras.preprocessing import image
 import os
 
-classifier = keras.models.load_model("model_face_1.h5")
+classifier = keras.models.load_model("model_face_cat.h5")
 img_dim_x = 100
 img_dim_y = 100
 
@@ -21,35 +21,61 @@ def i_pred (full_name):
     #print (result)
     return result
 
+def face_predictor(full_name,):
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    result = []
+    try:
+        img = cv2.imread(full_name)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            if x and y <= 20 : continue
+            temp_img = img.copy()
+            temp_img = temp_img[y:y + h, x:x + w]  # NOTE: its img[y: y + h, x: x + w]
+            temp_img = cv2.resize(temp_img, (img_dim_x, img_dim_y))
+            temp_img = cv2.cvtColor(temp_img, cv2.COLOR_BGR2RGB)
+            temp_img = np.expand_dims(temp_img, axis=0)
+            prediction  = classifier.predict(temp_img)
+            result.append(prediction)
+
+
+    finally:
+        return result
+
+
+
+
+
+
 
 
 path = 'dataset/single_prediction/'
+
 
 total = []
 
 
 
 for filename in os.listdir(path):
-    result = i_pred(path+filename)
+    result = face_predictor(path+filename)
+    prediction = "another"
 
-    if result[0][0] == 1:
-       prediction = 'woman'
-       total.append(prediction)
-    else :
-        prediction = 'man'
-        total.append(prediction)
+    for pred in result:
+        print (pred)
 
-    print (filename,prediction)
+        if pred[0][1] == 1:
+           prediction = 'woman'
+           total.append(prediction)
+        elif pred[0][0] == 1:
+            prediction = 'man'
+            total.append(prediction)
+        else :
+            prediction = 'another'
+            total.append(prediction)
+        print (filename,prediction)
 
 print ("w = ",total.count("woman"))
 
 print ("m = ",total.count("man"))
 
-#cathegorical
-w =  328
-m =  168
-
-# sparse
-
-w =  359
-m =  143
+print ("a = ",total.count("another"))
